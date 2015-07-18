282b1e6d5d38f078fe831a05fde1f62c5baa6acf
sanzunonyasama <sanzunonyasama@gmail.com>
2015-07-19 01:40:35 +0800

    komachi

--- cloud/main.js
+++ cloud/main.js
@@ -47,7 +47,7 @@ var cache = {};
 
 function send404(response){
     response.writeHead(404,{'Content-Type': 'text/plain'});
-    response.write('Error 404: resource not found!!!!!!');
+    response.write('Error 404: resource not found.');
     response.end();
 }
 
