12fb1f0912b47a6b53988555f2b073e9abfe0b95
sanzunonyasama <sanzunonyasama@gmail.com>
2015-07-19 01:10:15 +0800

    komachi

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-var name = require('cloud/name.js');
+/*var name = require('cloud/name.js');
 require('cloud/app.js')
 AV.Cloud.define("hello", function(request, response) {
     console.log(request.user);
@@ -35,3 +35,65 @@ AV.Cloud.afterSave("TestReview", function(request) {
 	});
 });
 */
+
+/**
+ * Created by kawagiri on 15/7/18.
+ */
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+function send404(response){
+    response.writeHead(404,{'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents){
+    response.writeHead(
+        200,
+        {"content-type":mime.lookup(path.basename(filePath))}
+    );
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath){
+    if(cache[absPath]){
+        sendFile(response, absPath, cache[absPath]);
+    } else{
+        fs.exists(absPath, function(exists){
+            if(exists){
+                fs.readFile(absPath, function(err, data){
+                    if(err){
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response,absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        })
+    }
+}
+
+var server = http.createServer(function(request, response){
+    var filePath = false;
+    if(request.url == '/'){
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function(){
+    console.log("Server listening on port 3000.");
+})
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
